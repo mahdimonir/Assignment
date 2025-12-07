f5d3851d78baecb18f81fb0d93df0d2d0f06baf4
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-12-07 21:09:53 +0600

    Remove process.exit to fix FUNCTION_INVOCATION_FAILED errors

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.use(
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
-// Initialize DB tables (non-blocking for serverless)
+// Initialize DB tables
 initDB()
   .then(() => {
     console.log("PostgreSQL connected & tables initialized");
