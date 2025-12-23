22d1c19a75dcb0cb6bcce418b2c16b84a67f7bd3
mahdimonir <mahdimoniruzzaman@gmail.com>
2025-12-23 21:02:11 +0600

    Setting up environment for assignment 03

--- src/modules/auth/auth.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { authController } from "./auth.controller";
-
-const router = Router();
-
-router.post("/signup", authController.signupUser);
-router.post("/signin", authController.signinUser);
-
-export const authRoutes = router;
